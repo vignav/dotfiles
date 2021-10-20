b2b688d759f728fff7c93ebef2e4a56903e14375
Vignav <vignav.ram@gmail.com>
2021-10-20 16:30:40 +0530

    fixed everything after breaking it

---
+++ install.sh
@@ -0,0 +1,29 @@
+if ! [  -f /etc/debian_version ]  ; then
+  echo "This is not Debian. Installation Aborted"
+  exit 1
+fi
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+BASE=$(pwd)
+#sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y
+
+if ! command -v fzf ; then
+  echo "fzf not found. Installing fzf now."
+  sudo apt install fzf -y
+else
+  echo "fzf found."
+fi
+
+cd
+BASE=$(dirname "${BASH_SOURCE[0]}")
+
+echo $BASE
+
+ln -sfv $BASE/.bashrc ~/.bashrc
+ln -sfv $BASE/vim ~/.vim
+ln -sfv $BASE/.bash_aliases ~/.bash_aliases
+
+echo "Sourcing .bashrc"
+source ~/.bashrc
+
+vim +PlugInstall +qall
