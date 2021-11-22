9021f440312a1674566271814f9183b093e3be26
Vignav <vignav.ram@gmail.com>
2021-11-22 16:36:59 +0530

    too many changes 	new file:   .bash_wsl 	modified:   .bashrc 	new file:   .bashrc_ohmybash 	modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,35 +1,31 @@
-if ! [  -f /etc/debian_version ]  ; then
-  echo "This is not Debian. Installation Aborted"
-  exit 1
-fi
-
 cd "$(dirname "${BASH_SOURCE[0]}")"
 BASE=$(pwd)
 
-sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
-
-if ! command -v fzf ; then
-  echo "fzf not found. Installing fzf now."
-  sudo apt install fzf -y
-else
-  echo "fzf found."
-fi
 
 ln -sfv $BASE/.bashrc ~/.bashrc
 ln -sTfv $BASE/vim ~/.vim
 ln -sfv $BASE/.bash_aliases ~/.bash_aliases
+ln -sfv $BASE/.bashrc_ohmybash ~/.bashrc_ohmybash
+
+vim +PlugInstall +qall
+
+#if it is wsl
+WSL=$(uname -a | grep 'WSL')
+if [ -n "$WSL" ]; then
+  sudo bash -c "echo -e '[network]\ngenerateResolvConf = false' > /etc/wsl.conf ; echo 'nameserver 1.1.1.1' > /etc/resolv.conf"
+  ln -sfv $BASE/.bash_wsl ~/.bash_wsl
+  echo "source ~/.bash_wsl" >> .bashrc
+fi
+
+#if it is debian
+if [  -f /etc/debian_version ]  ; then
+  if ! command -v fzf ; then
+    sudo apt install fzf -y
+  fi
+  sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y
+  sudo apt install locales-all
+fi
 
 echo "Sourcing .bashrc"
 source ~/.bashrc
-
-vim +PlugInstall +qall
-WSL=$(uname -a | grep 'WSL')
-if [ -n "$WSL" ]; then
-  echo "This is WSL. Setting up DNS server as 1.1.1.1"
-  sudo echo -e "[network]\ngenerateResolvConf = false" > /etc/wsl.conf
-  sudo echo "nameserver 1.1.1.1" > /etc/resolv.conf
-else
-  echo "This is not WSL"
-  sudo apt install locales-all
-fi
